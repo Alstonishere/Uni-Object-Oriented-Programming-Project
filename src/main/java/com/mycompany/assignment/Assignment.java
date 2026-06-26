ce4b89a3b90f6183f4b65dc31b2eac30e2c0ac9a
Alstonishere <alston.wongyongyi@gmail.com>
2026-06-26 14:27:45 +0800

    Delete src/main/java/com/mycompany/assignment directory

--- src/main/java/com/mycompany/assignment/Assignment.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.assignment;
-
-public class Assignment {
-
-    public static void main(String[] args) {
-        new Splash_Screen().setVisible(true); //main file to run the program
-    }
-}
